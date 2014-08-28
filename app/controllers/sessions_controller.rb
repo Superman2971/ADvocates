ae34e432d7f16476f231e0dbe931451fc0cc9c71
kgjacobsen89 <kgjacobsen89@gmail.com>
2014-08-28 15:29:48 -0700

    finally, all sessions are working :)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if @business = Business.new
+    current_uri = request.env['PATH_INFO']
+    if current_uri == "/session"
       @business = Business.where(name: params[:business][:name]).first
       if @business && @business.authenticate(params[:business][:password])
         session[:business_id] = @business.id.to_s
@@ -13,7 +14,7 @@ class SessionsController < ApplicationController
       else 
         redirect_to root_path
       end 
-    else 
+    else
       omniauth = env["omniauth.auth"] 
       user = User.find_or_create_with_omniauth(omniauth)
       session[:user_id] = user.id
