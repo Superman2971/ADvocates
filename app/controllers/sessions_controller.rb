5cd91cad33f9cde082200f1c69c74e1a6c4fa51d
Superman2971 <goldfarb.ian@gmail.com>
2014-08-28 15:13:03 -0700

    minor cleanup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,20 @@
 class SessionsController < ApplicationController
   def new
-    # Present an empty login form
     @business = Business.new
   end
 
   def create
-    if @business = Business.new
+    if Business.where(name: params[:business][:name]).first
       @business = Business.where(name: params[:business][:name]).first
       if @business && @business.authenticate(params[:business][:password])
         session[:business_id] = @business.id.to_s
         redirect_to business_path(@business.id)
       else 
         redirect_to root_path
-      end 
-    else 
+      end
+    else
       omniauth = env["omniauth.auth"] 
-      user = User.find_or_create_with_omniauth(omniauth)
+      user = User.find_or_create_with_omniauth(@omniauth)
       session[:user_id] = user.id
       redirect_to user_path(user.id), notice: "Signed in!"
     end
@@ -25,5 +24,4 @@ class SessionsController < ApplicationController
     reset_session
     redirect_to root_path
   end
-
 end
