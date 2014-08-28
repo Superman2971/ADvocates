ad3c6459f46de0ce9feb24b728a344b715ee77b2
xypalmer <xypalmer@icloud.com>
2014-08-28 15:50:04 -0700

    added modals

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,10 @@
 class SessionsController < ApplicationController
   def new
-    # Present an empty login form
     @business = Business.new
   end
 
   def create
-    if @business = Business.new
+    if @business == Business.new || params[:test] == "test"
       @business = Business.where(name: params[:business][:name]).first
       if @business && @business.authenticate(params[:business][:password])
         session[:business_id] = @business.id.to_s
@@ -13,7 +12,7 @@ class SessionsController < ApplicationController
       else 
         redirect_to root_path
       end 
-    else 
+    else
       omniauth = env["omniauth.auth"] 
       user = User.find_or_create_with_omniauth(omniauth)
       session[:user_id] = user.id
