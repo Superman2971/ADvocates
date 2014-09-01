695b12732027a62cc9cc926c7233a837372a36ce
Superman2971 <goldfarb.ian@gmail.com>
2014-08-31 21:30:29 -0700

    17 model rspecs added and validations for models

--- spec/controllers/business_controller_spec.rb
+++ spec/controllers/business_controller_spec.rb
@@ -1,38 +1,38 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe BusinessesController do 
-# 	let :valid_attributes do
-# 		{
-# 			name: "Wayne Enterprises",
-# 			email: "bwayne@wayne.com",
-# 			password: "ImBatman",
-# 		}
-# 	end
+describe BusinessesController do 
+	let :valid_attributes do
+		{
+			name: "Wayne Enterprises",
+			email: "bwayne@wayne.com",
+			password: "ImBatman",
+		}
+	end
 
-# 	describe "GET #index" do
-# 		let :business1 do
-# 			Business.create! valid_attributes
-# 		end
-# 		let :business2 do
-# 			Business.create! valid_attributes
-# 		end
+	describe "GET #index" do
+		let :business1 do
+			Business.create! valid_attributes
+		end
+		let :business2 do
+			Business.create! valid_attributes
+		end
 
-# 		before do
-# 			get :index
-# 		end
+		before do
+			get :index
+		end
 
-# 		it "should render the index template" do
-# 			expect(response).to render_template :index
-# 		end
+		it "should render the index template" do
+			expect(response).to render_template :index
+		end
 
-# 		it "should succeed" do
-# 			expect(response).to be_success
-# 		end
+		it "should succeed" do
+			expect(response).to be_success
+		end
 
-# 		it "should assign @businesses to be all businesses" do
-# 			expect(assigns(:businesses)).to include(:business1, :business2)
-# 		end
-# 	end
+		it "should assign @businesses to be all businesses" do
+			expect(assigns(:businesses)).to include(:business1, :business2)
+		end
+	end
 
 
-# end
+end
