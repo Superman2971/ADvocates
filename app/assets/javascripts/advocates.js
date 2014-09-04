9c7ae9415c8547d6b649f7aaaf791f2544d9935d
Superman2971 <goldfarb.ian@gmail.com>
2014-09-03 19:54:55 -0700

    json now showing on campaigns index, need to push to other views

--- app/assets/javascripts/advocates.js
+++ app/assets/javascripts/advocates.js
@@ -1,5 +1,28 @@
-adApp = angular.module("adApp", []);
+var adApp = angular.module("adApp",["ngResource"]);
 
-adApp.controller("adController", function($scope){
+// adApp.config(
+//   ['$httpProvider', function($httpProvider) {
+//   var authToken = angular.element("meta[name=\"csrf-token\"]").attr("content");
+//   var defaults = $httpProvider.defaults.headers;
+//   defaults.common["X-CSRF-TOKEN"] = authToken;
+//   defaults.patch = defaults.patch || {};
+//   defaults.patch['Content-Type'] = 'application/json';
+// }]);
 
-});
+adApp.factory("Campaign", ["$resource", function($resource){
+  return $resource("/api/campaigns/:id",
+  {id: "@id"},
+  {update: {method: "PATCH"}});
+}]);
+
+adApp.controller("adController", ["$scope", "Campaign", function($scope, Campaign){
+
+  $scope.test = "IT WORKS!";
+
+  $scope.newCampaign = new Campaign();
+
+  Campaign.query(function(json){
+    $scope.campaigns = json;
+  });
+
+}]);
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   
   resources :charges
 
+  #json API
+  scope "/api" do
+    resources :campaigns, defaults: { format: :json }
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
