8ce508586c0f44af4ad8b7cc708a24df979718ac
kgjacobsen89 <kgjacobsen89@gmail.com>
2014-09-04 15:34:57 -0700

    read me, RSPEC still not working for campaign

---
+++ spec/factories/business.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do 
+	factory :business do
+		name "Test"
+		email "Test@testing.com"
+		password "Testing"
+		tweets 100
+		id 1
+	end
+	
+end
--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -1,28 +1,29 @@
 require "spec_helper"
 
 describe Business do
-  before do
-    @business = Business.create!(name:"Test",email:"Test@gmail.com",password:"123456456",tweets:0)
-    @no_name = Business.new(email:"Test@gmail.com",password:"123456",tweets:0)
-    @no_email = Business.new(name:"Test",password:"123456",tweets:0)
-    @no_password = Business.new(name:"Test",email:"Test@gmail.com",tweets:0)
-  end
-  
-  it "should allow me to create a new business" do
-    expect(@business).to be_valid
-    expect(Business.count).to eq(1)
+  it "is valid with name, email, password, and tweets" do
+    business = FactoryGirl.build(:business)
+    expect(business).to be_valid
   end
 
   it "is invalid without a name" do
-    expect(@no_name).to be_invalid
+    business = FactoryGirl.build(:business, name: nil)
+    expect(business).to be_invalid
   end
   
   it "is invalid without an email" do
-    expect(@no_email).to be_invalid
+    business = FactoryGirl.build(:business, email: nil)
+    expect(business).to be_invalid
   end
   
   it "is invalid without a password" do
-    expect(@no_password).to be_invalid
+    business = FactoryGirl.build(:business, password: nil)
+    expect(business).to be_invalid
+  end
+
+  it "should have many tweets" do
+    business = FactoryGirl.build(:business)
+    expect(business.tweets).to eq(100)
   end
 
   it "should have many Campaigns" do
